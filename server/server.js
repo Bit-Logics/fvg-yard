8c5d885b4d3d1b3b9ef1ac5a7ab93e6dcc0742f2
BitLogics <noncheatto@gmail.com>
2026-06-20 12:12:29 +0200

    Fix wildcard route for Express

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ gameLogic.init(io);
 // Serve static React files in production
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
